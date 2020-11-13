333d0e9a74f3537ef49a2123479ffdeb229788a2
Matin <matinabbasi@yandex.com>
2020-11-13 15:41:07 +0330

    seccend commit in commit dinimic web site in a dinike background

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("test");
+let R_matin;
+R_matin = Math.random() * 11;
+R_matin = Math.floor(R_matin);
+document.getElementById("st").innerHTML = " <style> body{ height: 100vh; background: url(back" + R_matin + ".jpg) no-repeat center; background-size: cover; display: flex; align-items: center; justify-content: center; } </style>  ";
+
+
